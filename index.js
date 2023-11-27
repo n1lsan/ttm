b972c8ef82e8a9061eaa39f738e1c4b4c1990d16
n1lsan <nikita.tyutenkov@gmail.com>
2023-11-27 12:25:56 +0300

    logs for test

--- index.js
+++ index.js
@@ -2,18 +2,30 @@
 
 const express = require('express');
 // eslint-disable-next-line import/no-extraneous-dependencies
-const { PrismaClient } = require('@prisma/client');
+// const { PrismaClient } = require('@prisma/client');
+const bodyParser = require('body-parser')
 
-const prisma = new PrismaClient();
+// const prisma = new PrismaClient();
 const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true,
+}));
 
 app.get('/', async (req, res) => {
+  res.status(200).json({
+    message: 'Hello world!',
+  });
+});
+
+app.post('/api/v1/youtrack/minerva', async (req, res) => {
   try {
-    const allusers = await prisma.user.findMany();
-    res.json(allusers);
+    // const allusers = await prisma.user.findMany();
+    console.log(req.body);
+    return res.json(req.body);
   } catch (error) {
     console.log(error.message);
-    res.status(500).json({
+    return res.status(500).json({
       message: 'Internal Server Error',
     });
   }
